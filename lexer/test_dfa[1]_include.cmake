5f0f30aab5367d5da63158eedc20c5f7aaf272b2
David Simonetti <22davidsimonetti22@gmail.com>
2024-07-15 22:10:42 -0700

    Drop files from .gitignore

--- lexer/test_dfa[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/davidsim/Documents/Programming/IntegratedCompiler/lexer/test_dfa[1]_tests.cmake")
-  include("/home/davidsim/Documents/Programming/IntegratedCompiler/lexer/test_dfa[1]_tests.cmake")
-else()
-  add_test(test_dfa_NOT_BUILT test_dfa_NOT_BUILT)
-endif()
